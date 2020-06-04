0c0e904869adfe72408fd1e2ea07e8c4fd34e9b1
sophiasagan <sophiabraddock82@gmail.com>
2020-06-03 20:10:31 -0400

    12 complete

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(int1, int2):
+    return int1 + int2
+
 
 print(f1(1, 2))
 
@@ -14,6 +17,11 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*args):
+    print(sum(args))
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,7 +30,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(sum(a)))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +38,9 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+def f3(x, y=1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -45,6 +56,10 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+def f4(**kwargs):
+    for kwarg in kwargs:
+        print(f'key: {kwarg}, value: {kwargs[kwarg]}')
+
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -56,10 +71,10 @@ f4(a=12, b=30)
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
+d={
     "monster": "goblin",
     "hp": 3
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
